c77a02d33a6983d30d411e5deaafbe8c1d116d11
Dmitriy <xmokeri@protonmail.com>
2024-06-18 15:58:38 +0300

    some updates

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
-import random
+# Из списка удалить четные элементы, стоящие между максимальным и минимальным элементами.
+# Пример: из списка A[7]: 1 8 8 4 7 0 5 должен получиться список A[5]: 1 8 7 0 5.
+
+import random as r
 
 def get_list():
-    # Запрашиваем у пользователя способ ввода списка и возвращаем список чисел
+    # Запрашиваем у пользователя данные
     choice = input("Выберите способ ввода списка:\n"
                    "1. Ввести вручную\n"
                    "2. Сгенерировать автоматически\n"
@@ -10,32 +13,31 @@ def get_list():
         # Ввод списка вручную
         numbers_str = input("Введите элементы списка через пробел: ")
         try:
-            lst = [int(x) for x in numbers_str.split()]
+            lst = [a for a in numbers_str.split()]
         except ValueError:
             print("Некорректный ввод. Введите числа через пробел.")
             return get_list()
     elif choice == '2':
-        # Автоматическая генерация списка
+        # Ввод списка автоматически
         n = int(input("Введите количество элементов списка: "))
-        min_value = 0
-        max_value = 9
-        lst = [random.randint(min_value, max_value) for _ in range(n)]
+        lst = [r.randint(0, 9) for _ in range(n)]
     else:
         print("Неверный выбор. Попробуйте снова.")
         return get_list()
     return lst
 
 def remove(lst):
-  # Удаляем четные элементы, стоящие между максимальным и минимальным элементами списка
-  min_index = lst.index(min(lst))
-  max_index = lst.index(max(lst))
-  start, end = min(min_index, max_index) + 1, max(min_index, max_index)
-  lst[start:end] = [x for x in lst[start:end] if x % 2 != 0]
+    # Удаляем четные элементы, стоящие между максимальным и минимальным элементами списка
+    min_index = lst.index(min(lst))
+    max_index = lst.index(max(lst))
+    start, end = min(min_index, max_index) + 1, max(min_index, max_index)
+    lst[start:end] = [х for х in lst[start:end] if х % 2 != 0]
+    return lst
 
 # Основная программа
 lst = get_list()
 print(f"Исходный список: {lst}")
-result = remove(lst)
+lst = remove(lst)
 print(f"Результат: {lst}")
 
 
--- 2.py
+++ 2.py
@@ -1,11 +1,12 @@
+# Выполнить обработку элементов прямоугольной матрицы A, имеющей N строк и M столбцов. Все элементы имеют целый тип.
+# Дано целое число H. Определить, какие столбцы имеют хотя бы одно такое число, а какие не имеют.
+
 import numpy as np
 
 # Задаем размер матрицы
 N = int(input("Введите количество строк матрицы (N): "))
 M = int(input("Введите количество столбцов матрицы (M): "))
-# Генерируем матрицу A со случайными целыми числами
-A = np.random.randint(0, 9, size=(N, M))
-# Вводим число H для поиска
+A = np.random.randint(0, 10, size=(N, M))
 H = int(input("Введите число H: "))
 # Находим столбцы, содержащие число H
 columns_with_H = np.any(A == H, axis=0)
@@ -13,15 +14,15 @@ columns_with_H = np.any(A == H, axis=0)
 columns_without_H = np.logical_not(columns_with_H)
 
 # Сохраняем результаты в файл
-with open("result.txt", "w") as f:
-    f.write("Исходная матрица A:\n")
-    f.write(str(A))
-    f.write("\nЧисло H: ")
-    f.write(str(H))
-    f.write("\nНомера столбцов, содержащих H:\n")
-    f.write(str(np.where(columns_with_H)[0]))
-    f.write("\nНомера столбцов, не содержащих H:\n")
-    f.write(str(np.where(columns_without_H)[0]))
+with open("result.txt", "w") as file:
+    file.write("Исходная матрица A:\n")
+    file.write(str(A))
+    file.write("\nЧисло H: ")
+    file.write(str(H))
+    file.write("\nНомера столбцов, содержащих H:\n")
+    file.write(str(np.where(columns_with_H)[0]))
+    file.write("\nНомера столбцов, не содержащих H:\n")
+    file.write(str(np.where(columns_without_H)[0]))
 
 # Выводим результаты на экран
 print("Исходная матрица A:\n", A)
